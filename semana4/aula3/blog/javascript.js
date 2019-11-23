65e77f1a3951e63ad78ab74191b44f8d1d8f9019
Bianca <gerez.biancaa@gmail.com>
2019-11-23 19:01:29 -0200

    Não consegui fazer o passo 4 funcionar, passo 5 concluido

--- semana4/aula3/blog/javascript.js
+++ semana4/aula3/blog/javascript.js
@@ -5,22 +5,23 @@ class criarNovoPost {
     this.conteudo = conteudo
 }
 }
-
+console.log(criarNovoPost)
 function enviarPost (){
-    let tituloValue = document.getElementById("titulo").value;
-    let autorValue = document.getElementById("autor").value
-    let conteudoValue = document.getElementById("conteudo").value
-   
-    let arrayEnviarPost = new criarNovoPost (tituloValue,autorValue,conteudoValue)
-   
-    arrayDadosPost.push(arrayEnviarPost)
+
+let tituloValue = document.getElementById("titulo").value
+let autorValue = document.getElementById("autor").value
+let conteudoValue = document.getElementById("conteudo").value
+let arrayPost = new criarNovoPost (tituloValue,  autorValue , conteudoValue)
+
+    post.innerHTML += "<div > <br><h3>" + tituloValue + "</h3>"+ "<br> <p> Escrito por:   " + "" +
+    autorValue + "</p>" + "<br> <p>  " + conteudoValue + "<br><p> </div>";
+
+    arrayPostDados.push(arrayPost)
     document.getElementById("titulo").value = ""
     document.getElementById("autor").value = ""
     document.getElementById("conteudo").value = ""
-    console.log(arrayDadosPost)
-    postar()
+   
 }
+console.log(arrayPost)
 
-function postar (){
-    
-}
+
